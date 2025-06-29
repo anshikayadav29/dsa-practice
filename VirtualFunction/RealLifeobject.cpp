645ff7c9c6ae4c8944a8256aa01e5d7f28590078
anshikayadav11 <anshikayadav2917@gmail.com>
2025-06-29 11:03:29 +0530

    commit

--- VirtualFunction/RealLifeobject.cpp
+++ VirtualFunction/RealLifeobject.cpp
@@ -1,20 +1,17 @@
 #include <iostream>
 using namespace std;
-
 class Employee {
 public:
     virtual void role() {
         cout << "Generic Employee\n";
     }
 };
-
 class Developer : public Employee {
 public:
     void role() override {
         cout << "Writes code\n";
     }
 };
-
 class Manager : public Employee {
 public:
     void role() override {
