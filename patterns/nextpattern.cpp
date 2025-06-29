6268f8c905ca53308410bc026eec1c992fd76e07
anshikayadav11 <anshikayadav2917@gmail.com>
2025-06-29 22:08:30 +0530

    commit

---
+++ patterns/nextpattern.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n; // Input: 7
+
+    for (int i = 0; i < n; i++) {
+        // Print leading spaces
+        for (int j = 0; j < i; j++) {
+            cout << "  "; // Two spaces for alignment
+        }
+
+        // Print numbers
+        for (int j = 1; j <= n - i; j++) {
+            cout << j << " ";
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
