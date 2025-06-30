f85264fb6832f25fdb542d3b6450136843cd4519
anshikayadav11 <anshikayadav2917@gmail.com>
2025-06-30 08:47:09 +0530

    commit

---
+++ RuntimePolymorphism/VirtualFunctionwithConstructor.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+class Base {
+public:
+    Base() {
+        cout << "Base constructor\n";
+    }
+    virtual ~Base() {
+        cout << "Base destructor\n";
+    }
+};
+
+class Derived : public Base {
+public:
+    Derived() {
+        cout << "Derived constructor\n";
+    }
+    ~Derived() {
+        cout << "Derived destructor\n";
+    }
+};
+
+int main() {
+    Base* b = new Derived();
+    delete b;
+    return 0;
+}
