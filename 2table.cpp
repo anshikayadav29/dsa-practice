3df83e131736acaa35012091b2a163bfde06c4f1
anshikayadav11 <anshikayadav2917@gmail.com>
2025-07-02 09:48:43 +0530

    commit

--- 2table.cpp
+++ 2table.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <iostream>
 using namespace std;
 
@@ -11,18 +10,4 @@ int main() {
     int num = 5;
     printTable(num);
     return 0;
-=======
-#include <iostream>
-using namespace std;
-
-void printTable(int n) {
-    for (int i = 1; i <= 10; i++)
-        cout << n << " x " << i << " = " << n * i << endl;
-}
-
-int main() {
-    int num = 5;
-    printTable(num);
-    return 0;
->>>>>>> 71389b6cdb03b02d56bd4dbce0e8dc2ecf76e24f
 }
