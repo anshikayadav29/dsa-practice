462e4f46b0b6016a58f407e85cbb4bb565c749d1
anshikayadav11 <anshikayadav2917@gmail.com>
2025-07-03 08:07:42 +0530

    commit

--- 2swapnumber.cpp
+++ 2swapnumber.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include<iostream>
 using namespace std;
 
@@ -13,19 +12,3 @@ int main (){
     cout<<"After swap: a ="<< a<<", b=" <<b;
     return 0;
 }
-=======
-#include<iostream>
-using namespace std;
-
-void swap(int &x, int &y){
-    int temp =x;
-    x =y;
-    y=temp;
-}
-int main (){
-    int a =2, b=3;
-    swap (a, b);
-    cout<<"After swap: a ="<< a<<", b=" <<b;
-    return 0;
-}
->>>>>>> 71389b6cdb03b02d56bd4dbce0e8dc2ecf76e24f
