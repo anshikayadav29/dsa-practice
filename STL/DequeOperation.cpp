a0d20d5ef52af8d0b0b7f543913c4415198dfc5a
anshikayadav11 <anshikayadav2917@gmail.com>
2025-07-04 15:09:20 +0530

    commit

---
+++ STL/DequeOperation.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <deque>
+using namespace std;
+
+int main() {
+    deque<int> d;
+    d.push_back(1);
+    d.push_front(2);
+    cout << d.front() << " " << d.back();
+    return 0;
+}
