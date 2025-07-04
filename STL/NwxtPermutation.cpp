85bde65456f1470c370c9fa490039b310e6624fc
anshikayadav11 <anshikayadav2917@gmail.com>
2025-07-04 14:40:52 +0530

    commit

---
+++ STL/NwxtPermutation.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main() {
+    vector<int> v = {1, 2, 3};
+    next_permutation(v.begin(), v.end());
+    for(int x : v) cout << x << " ";
+    return 0;
+}
--- STL/countwithcount.cpp
+++ STL/countwithcount.cpp
@@ -7,4 +7,4 @@ int main() {
     vector<int> v = {1, 2, 2, 3};
     cout << count(v.begin(), v.end(), 2);
     return 0;
-}
+} 
