7d320e6903314f4ce3b0302faebcef9615da0a0d
anshikayadav11 <anshikayadav2917@gmail.com>
2025-08-13 00:50:51 +0530

    commit

---
+++ RECURSION2/GCD2.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+int gcd(int a,int b){return (b==0)?a:gcd(b,a%b);}
+int main(){
+    int a,b;
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+    cout << "GCD = " << gcd(a,b);
+}
