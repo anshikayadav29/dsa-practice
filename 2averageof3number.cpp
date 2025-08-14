109b5d714acfc2f7a9d427f829d4857681875584
anshikayadav11 <anshikayadav2917@gmail.com>
2025-08-15 00:05:03 +0530

    commit

--- 2averageof3number.cpp
+++ 2averageof3number.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <iostream>
 using namespace std;
 
@@ -9,16 +8,4 @@ float average(int a, int b, int c) {
 int main() {
     cout << "Average: " << average(3, 6, 9);
     return 0;
-=======
-#include <iostream>
-using namespace std;
-
-float average(int a, int b, int c) {
-    return (a + b + c) / 3.0;
-}
-
-int main() {
-    cout << "Average: " << average(3, 6, 9);
-    return 0;
->>>>>>> 71389b6cdb03b02d56bd4dbce0e8dc2ecf76e24f
 }
