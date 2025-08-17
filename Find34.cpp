52bd068a7556ce0235af4b7b7a1ff2ffb5e65b0f
anshikayadav29 <anshikayadav2917@gmail.com>
2025-08-17 12:27:23 +0530

    Commit

---
+++ Find34.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter size of array: ";
+    cin >> n;
+
+    vector<int> arr(n);
+    cout << "Enter elements: ";
+    for (int i = 0; i < n; i++) cin >> arr[i];
+
+    int maxi = arr[0];
+    for (int i = 1; i < n; i++) {
+        maxi = max(maxi, arr[i]);
+    }
+
+    cout << "Maximum element = " << maxi << endl;
+    return 0;
+}
